db8e58222b44340f22262f7ecec7c3b9aa1b6690
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 08:00:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-    let submit = document.getElementById('create-task-form');
-    submit.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-
+   let create-task-form=document.getElementById('create-task-form');
+   create-task-form.addEventListener('submit',(event) => {
+      event.preventDefault();
+   
       const task-description-input=document.getElementById('new-task-description').value;
       const myTasksLists = document.getElementById('tasks');
       const newListItem= document.createElement(li);
-
+   
       newListItem.innerHtml=task-description-input;
        myTasksLists.appendChild(newListItem);
           console.log(myTasksLists);
    });
-  // let submit = document.getElementById('create-task-form');
-  // submit.addEventListener('submit', (e) => {
-  //   e.preventDefault();
-  //
-  //   let input = document.getElementById("new-task-description").value;
-  //    let list = document.querySelector("#tasks");
-  //    let li = document.createElement('li');
-  //    li.innerHTML =`${input} `;
-  //    list.appendChild(li);
-  //    console.log(list);
-  //
-  //
-  // });
+  let submit = document.getElementById('create-task-form');
+  submit.addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+    let input = document.getElementById("new-task-description").value;
+     let list = document.querySelector("#tasks");
+     let li = document.createElement('li');
+     li.innerHTML =`${input} `;
+     list.appendChild(li);
+     console.log(list);
+  
+  
+  });
 });
