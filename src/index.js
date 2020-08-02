74ca18b737a7bbeecac655a846385cbc0af108af
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 08:11:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const task_description_input=document.getElementById('new-task-description').value;
       const  my_tasks_lists = document.getElementById('tasks');
       const new_listItem= document.createElement('li');
-      new_listItem.innerHTML=task_description_input.value;
+      new_listItem.innerHTML=task_description_input;
        my_tasks_lists.appendChild(new_listItem);
           console.log(my_tasks_lists);
    });
