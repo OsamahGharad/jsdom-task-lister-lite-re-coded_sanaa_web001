afdc9826f6da2c5ad1dc732374f6803e8ec29bfb
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 07:16:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let input=document.getElementById(input);
-   let submit=document.getElementById('submit');
+   let submit=document.getElementById('create-task-form');
    let myList = document.createElement('ul');
    body.appendChild(myList);
+   
    submit.addEventListener("submit",function(event){
       event.preventDefault();
        document.createElement(li);
