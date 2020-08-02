309bf26590383bcdd05cf1859b85a2ae0467012d
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 07:28:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-   const task-descriptionInput=document.getElementById('new-task-description');
+   const task-description-input=document.getElementById('new-task-description');
    const create-task-form=document.getElementById('create-task-form');
    const myTasksLists = document.getElementById('tasks');
 
    create-task-form.addEventListener("submit",function(event){
       event.preventDefault();
       const newListItem= document.createElement(li);
-      newListItem.innerHtml=task-descriptionInput.value;
+      newListItem.innerHtml=task-description-input.value;
        myTasksLists.appendChild(newListItem);
 
 
