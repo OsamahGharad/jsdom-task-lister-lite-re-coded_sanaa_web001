d26aa6f55e2ab02117db137b1b21dc5d226acd86
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 07:35:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  //  const task-description-input=document.getElementById('new-task-description');
-  //  const create-task-form=document.getElementById('create-task-form');
-  //  const myTasksLists = document.getElementById('tasks');
-   //
-  //  create-task-form.addEventListener("submit", function(event){
-  //     event.preventDefault();
-  //     const newListItem= document.createElement(li);
-  //     newListItem.innerHtml=task-description-input.value;
-  //      myTasksLists.appendChild(newListItem);
-  //  });
-  let submit = document.getElementById('create-task-form');
-  submit.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    let input = document.getElementById("new-task-description").value;
-     let list = document.querySelector("#tasks");
-     let li = document.createElement('li');
-     li.innerHTML =`${input} `;
-     list.appendChild(li);
-     console.log(list);
-
-
-  });
+   const task-description-input=document.getElementById('new-task-description');
+   const create-task-form=document.getElementById('create-task-form');
+   const myTasksLists = document.getElementById('tasks');
+   
+   create-task-form.addEventListener("submit",(event) => {
+      event.preventDefault();
+      const newListItem= document.createElement(li);
+      newListItem.innerHtml=task-description-input.value;
+       myTasksLists.appendChild(newListItem);
+          console.log(myTasksLists);
+   });
+  // let submit = document.getElementById('create-task-form');
+  // submit.addEventListener('submit', (e) => {
+  //   e.preventDefault();
+  // 
+  //   let input = document.getElementById("new-task-description").value;
+  //    let list = document.querySelector("#tasks");
+  //    let li = document.createElement('li');
+  //    li.innerHTML =`${input} `;
+  //    list.appendChild(li);
+  //    console.log(list);
+  // 
+  // 
+  // });
 });
