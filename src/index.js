247b6cd2a9941a3ea813c68d611789fa2b86189f
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 07:35:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
    const task-description-input=document.getElementById('new-task-description');
    const create-task-form=document.getElementById('create-task-form');
    const myTasksLists = document.getElementById('tasks');
-   
+
    create-task-form.addEventListener("submit",(event) => {
       event.preventDefault();
       const newListItem= document.createElement(li);
@@ -14,14 +14,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // let submit = document.getElementById('create-task-form');
   // submit.addEventListener('submit', (e) => {
   //   e.preventDefault();
-  // 
+  //
   //   let input = document.getElementById("new-task-description").value;
   //    let list = document.querySelector("#tasks");
   //    let li = document.createElement('li');
   //    li.innerHTML =`${input} `;
   //    list.appendChild(li);
   //    console.log(list);
-  // 
-  // 
+  //
+  //
   // });
 });
