9e4037b40c1e44b746d8d6b2685d930a545d8af7
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 08:01:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -15,14 +15,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // let submit = document.getElementById('create-task-form');
   // submit.addEventListener('submit', (e) => {
   //   e.preventDefault();
-  // 
+  //
   //   let input = document.getElementById("new-task-description").value;
   //    let list = document.querySelector("#tasks");
   //    let li = document.createElement('li');
   //    li.innerHTML =`${input} `;
   //    list.appendChild(li);
   //    console.log(list);
-  // 
-  // 
+  //
+  //
   // });
 });
