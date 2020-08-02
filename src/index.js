8286c74d3265b25e0c74b109be2b5d8285f2a632
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 08:16:29 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+   let submitForm=document.getElementById('create-task-form');
+   submitForm.addEventListener('submit',function(event) {
+      event.preventDefault();
+
+      let task_description_input=document.getElementById('new-task-description').value;
+      const  my_tasks_lists = document.getElementById('tasks');
+      const new_listItem= document.createElement('li');
+      new_listItem.innerHTML=task_description_input;
+       my_tasks_lists.appendChild(new_listItem);
+          console.log(my_tasks_lists);
+             task_description_input=" ";
+             event.target.reset();
+   });
+
 });
