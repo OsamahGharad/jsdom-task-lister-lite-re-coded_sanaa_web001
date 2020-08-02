055866f980168a817b8839b2f38f4bdf9e6e28f7
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 07:14:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let input=document.getElementById(input);
+   let submit=document.getElementById('submit');
+   let myList = document.createElement('ul');
+   body.appendChild(myList);
+   submit.addEventListener("submit",function(event){
+      event.preventDefault();
+       document.createElement(li);
+       li.appendChild(ul);
+       li.innerHtml+=input;
+      
+   },false);
 });
